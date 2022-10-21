c5ce93b166c362821f04e2361db78eee6b859826
Jesse Dorman <Jesse.dorman@us.af.mil>
2022-10-21 14:41:54 +0000

    fix small issue

--- wx_app.py
+++ wx_app.py
@@ -56,7 +56,7 @@ class MainWindow(MDWidget):
 
 class Wx(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Green"
         return Builder.load_file('layouts.kv')
 
