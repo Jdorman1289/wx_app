7b68feffceda039bd996eb8a1fc5b9db445f42a1
Jesse Dorman <jdorman1289@gmail.com>
2022-10-22 07:26:01 -0500

    fix error that crashed program

--- wx_app.py
+++ wx_app.py
@@ -14,6 +14,7 @@ Window.size = 640, 640
 class MainWindow(MDWidget):
 
     def get_wx(self):
+
         # grabs database of locations
         
         cities = pd.read_csv("uscities.csv")
@@ -26,21 +27,21 @@ class MainWindow(MDWidget):
         coordinates = city_coordinates[["lat", "lng"]].where(city_coordinates["state_name"] == state).dropna()
 
         check = str(coordinates.values).split()
-        lat = check[1]
-        lon = check[2].strip("]")
-        
-        url = f"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}"
+        if len(check) >= 3:
+            lat = check[1]
+            lon = check[2].strip("]")
+            
+            url = f"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}"
 
-        site = requests.get(url)
-        site_soup = bs(site.content, features="html.parser")
-        wx = site_soup.find_all("div", re.compile("forecast-text"))
-        time_label = site_soup.find_all("div", re.compile("forecast-label"))
+            site = requests.get(url)
+            site_soup = bs(site.content, features="html.parser")
+            wx = site_soup.find_all("div", re.compile("forecast-text"))
+            time_label = site_soup.find_all("div", re.compile("forecast-label"))
 
-        obs = site_soup.find_all("div", re.compile("pull-left"))
+            obs = site_soup.find_all("div", re.compile("pull-left"))
 
-        self.ids.observation.text = f"Current Conditions for {location} {state} {obs[0].text}{obs[1].text}"
-        self.ids.forecast.text = f"Forecast for {location} {state} \n\n{time_label[0].text}: {wx[0].text} \n\n{time_label[1].text}: {wx[1].text} \n\n{time_label[2].text}: {wx[2].text} \n\n{time_label[3].text}: {wx[3].text} \n\n{time_label[4].text}: {wx[4].text}\n\n{time_label[5].text}: {wx[5].text}\n\n"
-
+            self.ids.observation.text = f"Current Conditions for {location} {state} {obs[0].text}{obs[1].text}"
+            self.ids.forecast.text = f"Forecast for {location} {state} \n\n{time_label[0].text}: {wx[0].text} \n\n{time_label[1].text}: {wx[1].text} \n\n{time_label[2].text}: {wx[2].text} \n\n{time_label[3].text}: {wx[3].text} \n\n{time_label[4].text}: {wx[4].text}\n\n{time_label[5].text}: {wx[5].text}\n\n"
 
     def clear(self):
         self.ids.location.text = ""
